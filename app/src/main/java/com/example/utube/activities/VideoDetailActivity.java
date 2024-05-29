f216074480db907047816b7cf14fd18ed25a104d
Yedpel <yedidya.peles@live.biu.ac.il>
2024-05-30 02:50:24 +0300

    all videos work with images in main

--- app/src/main/java/com/example/utube/activities/VideoDetailActivity.java
+++ app/src/main/java/com/example/utube/activities/VideoDetailActivity.java
@@ -2,12 +2,14 @@ package com.example.utube.activities;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.MediaController;
 import android.widget.TextView;
 import android.widget.VideoView;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.utube.R;
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
 public class VideoDetailActivity extends AppCompatActivity {
@@ -40,6 +42,9 @@ public class VideoDetailActivity extends AppCompatActivity {
         String uploadTime = getIntent().getStringExtra("UPLOAD_TIME");
         String authorProfilePicUrl = getIntent().getStringExtra("AUTHOR_PROFILE_PIC_URL");
 
+        // Log the URL for debugging
+        Log.d("VideoDetailActivity", "Author Profile Pic URL: " + authorProfilePicUrl);
+
         // Set video details
         if (videoUrl.startsWith("http")) {
             videoView.setVideoURI(Uri.parse(videoUrl));
@@ -52,11 +57,25 @@ public class VideoDetailActivity extends AppCompatActivity {
         viewsTextView.setText(views);
         uploadTimeTextView.setText(uploadTime);
 
-        // Load author's profile picture
-        Picasso.get().load(authorProfilePicUrl).into(authorProfilePic);
+        // Load author's profile picture with Picasso, set placeholder and error image
+        int authorProfilePicResId = getResources().getIdentifier(authorProfilePicUrl, "drawable", getPackageName());
+        Picasso.get()
+                .load(authorProfilePicResId)
+                .placeholder(R.drawable.placeholder_image)  // Replace with your placeholder image
+                .error(R.drawable.error_image)  // Replace with your error image
+                .into(authorProfilePic, new Callback() {
+                    @Override
+                    public void onSuccess() {
+                        Log.d("Picasso", "Image loaded successfully");
+                    }
+
+                    @Override
+                    public void onError(Exception e) {
+                        Log.e("Picasso", "Error loading image", e);
+                    }
+                });
 
         // Start video
         videoView.start();
     }
 }
-
