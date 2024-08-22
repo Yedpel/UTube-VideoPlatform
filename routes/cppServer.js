02345c612015d259a43988db39b50c1e392f035d
Yedpel <yedidya.peles@live.biu.ac.il>
2024-08-22 17:59:26 +0300

    with comments on routes

--- routes/cppServer.js
+++ routes/cppServer.js
@@ -4,10 +4,17 @@ import { isLoggedIn } from '../middlewares/auth.js';
 
 const router = express.Router();
 
+//update the cp DB that the user has watched the video
+//the req need to have the video id and the token
 router.post('/notify-watch', isLoggedIn, notifyVideoWatch);
 
+//after authentication, create a thread for the user
+//the req need to have the token
 router.post('/create-thread', isLoggedIn, createUserThread);
 
+//before logging out, close the user thread
+//the req need to have the token
 router.post('/close-thread', isLoggedIn, closeUserThread);
+
 
 export default router;
